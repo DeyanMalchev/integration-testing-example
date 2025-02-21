77d62cd70d8e40781870d8eab064aa5e518f7059
DddeianM <116379803+DeyanMalchev@users.noreply.github.com>
2025-02-21 08:33:08 +0200

    Fixed spacing in code - order_module.py

--- order_module.py
+++ order_module.py
@@ -3,16 +3,14 @@ from discount_module import apply_discount
 from product_module import get_product
 
 def calculate_total_price(order_items):
-    """
-    Calculates the total price of an order.
-    """
+    
     total = 0
     for product_id, quantity in order_items:
         product = get_product(product_id)
         if product:
             total += product["price"] * quantity
 
-    # Apply discount
+    
     total = apply_discount(total)
 
     return total
